877b477effc1febbd455da60e7ddac35f2eb5335
jpmpl <lana.jpmpl@gmail.com>
2022-01-17 09:29:36 -0300

    first vertion: a_star + gvd

---
+++ catkin_ws/src/a_star/scripts/a_star.py
@@ -0,0 +1,226 @@
+#!/usr/bin/python3
+
+#SYSTEM
+from operator import is_not
+import sys
+
+#ROS
+import rospy
+from geometry_msgs.msg import Twist, Point, PoseStamped
+from sensor_msgs.msg import *
+from nav_msgs.msg import *
+from math import *
+
+#DATA STRUCTURE
+from queue import PriorityQueue
+
+#MATH
+from tf.transformations import euler_from_quaternion
+import numpy as np
+from scipy.signal import argrelextrema, find_peaks, peak_prominences
+import matplotlib.pyplot as plt
+
+#OPENCV
+import csv
+import cv2 as cv
+
+q0 = None
+qf = None
+theta = None
+lrange = None
+k = 10
+d = 0.5
+
+class Cell:
+
+    def __init__(self, row, col, value):
+        self.row = row
+        self.col = col
+        self.value = value
+    
+    def neighbors(self, grid):
+        neighbors = []
+        if self.row+1 <= grid.shape[0]:
+            neighbors.append(grid[self.row+1,self.col])
+        if self.row-1 > 0:
+            neighbors.append(grid[self.row-1,self.col])
+        if self.col+1 <= grid.shape[1]:
+            neighbors.append(grid[self.row,self.col+1])
+        if self.col-1 > 0:
+            neighbors.append(grid[self.row,self.col-1])
+        return neighbors
+
+class Prioritize:
+
+    def __init__(self, priority, item):
+        self.priority = priority
+        self.item = item
+
+    def __eq__(self, other):
+        return self.priority == other.priority
+
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+def odometry_callback_robot(data):
+    global q0, theta, k, d
+    orient = data.pose.pose.orientation
+    (roll, pitch, theta) = euler_from_quaternion([orient.x, orient.y, orient.z, orient.w])
+    x0 = data.pose.pose.position.x + d * cos(theta)
+    y0 = data.pose.pose.position.y + d * sin(theta)
+    q0 = np.array([x0, y0])
+
+def attraction_potential(qgoal):
+    di_threshold = 10
+    a = 5
+    di = np.linalg.norm(q0-qgoal)
+    if di <= di_threshold:
+        d_pot = a*(q0-qgoal)
+    else:
+        d_pot = di_threshold*a*(q0-qgoal)/di    
+    return d_pot
+
+def init():
+    rospy.init_node('a_star', anonymous=True)
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+    rospy.Subscriber('/odom', Odometry, odometry_callback_robot)
+    rate = rospy.Rate(20)
+    vel_msg = Twist()
+
+    if sys.version_info.major == 2:
+        qfx, qfy = raw_input('Digite as coordenadas do alvo (x,y): ').split()
+    elif sys.version_info.major == 3:
+        qfx, qfy = input('Digite as coordenadas do alvo (x,y): ').split()
+    
+    qfx, qfy = [float(i) for i in [qfx, qfy]]
+    qf = np.array([qfx,qfy])
+
+    img = cv.imread("./catkin_ws/src/a_star/worlds/circles.png")
+    width = img.shape[0]
+    height = img.shape[1]
+    GRID_SIZE = 50
+
+    ncols = int(width/GRID_SIZE)
+    nrows = int(height/GRID_SIZE)
+    grid = np.empty([nrows,ncols], Cell)
+    
+    WORLDSIZE = 80
+    GRID_SIZE_R = WORLDSIZE/ncols
+
+    while q0 is None:
+        continue
+
+    rng_x_img = range(0, width, GRID_SIZE)
+    rng_y_img = range(0, height, GRID_SIZE)
+    rng_x = [-WORLDSIZE/2+GRID_SIZE_R*x for x in range(0, ncols)]
+    rng_y = [WORLDSIZE/2-GRID_SIZE_R*y for y in range(0, nrows)]
+    ep = None
+    sp = None
+    for j in range(0,len(rng_y_img)):
+        for i in range(0,len(rng_x_img)):
+            cell_rect = img[rng_y_img[j]:rng_y_img[j]+GRID_SIZE,rng_x_img[i]:rng_x_img[i]+GRID_SIZE]
+            if not (np.sum(cell_rect==0) > 0):
+                grid[j,i] = Cell(j, i, 1)
+            else:
+                grid[j,i] = Cell(j, i, inf)
+            #print("i:{} j:{}".format(i, j))
+            #print("x_img:{} y_img:{}".format(rng_x_img[i], rng_y_img[j]))
+            #print("x:{} y:{}".format(rng_x[i], rng_y[j]))
+            #cv.imshow('img[{}][{}]'.format(i,j), cell_rect)
+            #cv.waitKey(0)
+            #cv.destroyAllWindows()
+            if q0[0]>=rng_x[i] and q0[0]<=rng_x[i]+GRID_SIZE_R and \
+                q0[1]<=rng_y[j] and q0[1]>=rng_y[j]-GRID_SIZE_R:
+                if grid[j,i].value is inf:
+                    raise RuntimeError('Start is not in a free cell')
+                sp = grid[j,i]
+            if qf[0]>=rng_x[i] and qf[0]<=rng_x[i]+GRID_SIZE_R and \
+                qf[1]<=rng_y[j] and qf[1]>=rng_y[j]-GRID_SIZE_R:
+                if grid[j,i].value is inf:
+                    raise RuntimeError('Goal is not in a free cell')
+                ep = grid[j,i]
+
+    if ep is None:
+        raise RuntimeError('Goal cell not found')
+    if sp is None:
+        raise RuntimeError('Start cell not found')
+
+    fronteir = PriorityQueue()
+    fronteir.put(Prioritize(0,sp))
+    cost_so_far = dict()
+    cost_so_far[sp] = 0
+    came_from = dict()
+    came_from[sp] = None
+
+    while not fronteir.empty():
+        cur_cell = fronteir.get().item
+        
+        if cur_cell.row == ep.row and cur_cell.col == ep.col:
+            break
+
+        for next in cur_cell.neighbors(grid):
+            new_cost = cost_so_far[cur_cell] + next.value
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + abs(ep.row - next.row) + abs(ep.col - next.col)
+                fronteir.put(Prioritize(priority,next))
+                came_from[next] = cur_cell
+
+    backtrace = []
+    backtrace.append(ep)
+    c_cell = ep
+    while came_from[c_cell] is not None:
+        c_cell = came_from[c_cell]
+        backtrace.append(c_cell)
+
+    # with open('data_wf_grid_25_13.csv','w', newline='') as csvfile:
+    #     fieldnames = ['row','col','x','y','size','isStart','isGoal','isFree','value']
+    #     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    #     writer.writeheader()
+    #     for j in range(0,grid.shape[0]):
+    #         for i in range(0,grid.shape[1]):
+    #             writer.writerow({'row':j,'col':i,'x':grid[j,i].x,'y':grid[j,i].y,\
+    #                 'size':GRID_SIZE_R,'isStart':grid[j,i].isStart,'isGoal':grid[j,i].isGoal,\
+    #                 'isFree':grid[j,i].isFree,'value': grid[j,i].value})
+
+    ep = 0.005
+    alp = 0.5
+    
+    # with open('data_wf_25_13.csv','w', newline='') as csvfile:
+    #     fieldnames = ['time','x','y','theta','Vx','Vy','Vlin','Vang',\
+    #         'grad_atr_pot_x','grad_atr_pot_y','targ_cell_row','targ_cell_col']
+    #     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    #     writer.writeheader()
+    cur_target = backtrace.pop()
+    cur_qf = np.array([rng_x[cur_target.col]+GRID_SIZE_R/2, rng_y[cur_target.row]-GRID_SIZE_R/2])
+    while not rospy.is_shutdown() and np.linalg.norm(q0-qf) > ep:
+        if q0 is not None and qf is not None and theta is not None:
+            if len(backtrace) > 0 and np.linalg.norm(q0-cur_qf) < 0.1:
+                cur_target = backtrace.pop()
+                cur_qf = np.array([rng_x[cur_target.col]+GRID_SIZE_R/2, rng_y[cur_target.row]-GRID_SIZE_R/2])
+                        
+            d_atr_pot = attraction_potential(cur_qf)
+            d_pot = d_atr_pot
+            V = - alp*(d_pot)
+                
+            # Omnidirectional robot
+            #vel_msg.linear.x = V[0]
+            #vel_msg.linear.y = V[1]
+            
+            # Diff robot
+            vel_msg.linear.x = cos(theta)*V[0]+sin(theta)*V[1]
+            vel_msg.angular.z = (-sin(theta)*V[0]+cos(theta)*V[1])/d
+
+            # writer.writerow({'time':rospy.Time.now(),'x':q0[0],'y':q0[1],\
+            #     'theta':theta,'Vx':V[0],'Vy':V[1],'Vlin':vel_msg.linear.x,\
+            #     'Vang':vel_msg.angular.z,'grad_atr_pot_x':d_atr_pot[0],\
+            #     'grad_atr_pot_y':d_atr_pot[1],'targ_cell_row':idx[0],'targ_cell_col':idx[1]})
+
+            rate.sleep()
+            pub.publish(vel_msg)
+    
+if __name__ == '__main__':
+    try:
+        init()
+    except rospy.ROSInterruptException:
+        pass
---
+++ catkin_ws/src/generalized_voronoi_diagram/scripts/gvd.py
@@ -0,0 +1,339 @@
+#!/usr/bin/python3
+
+#SYSTEM
+from configparser import ExtendedInterpolation
+from operator import is_not
+from re import L
+import sys
+
+#ROS
+import rospy
+from geometry_msgs.msg import Twist, Point, PoseStamped
+from sensor_msgs.msg import *
+from nav_msgs.msg import *
+from math import *
+
+#DATA STRUCTURE
+from queue import PriorityQueue
+
+#MATH
+from tf.transformations import euler_from_quaternion
+import numpy as np
+from scipy.signal import argrelextrema, find_peaks, peak_prominences
+from scipy.linalg import null_space
+import matplotlib.pyplot as plt
+from findpeaks import findpeaks
+import pandas as pd
+
+tangent_move = False
+q0 = None
+qf = None
+theta = None
+lrange = None
+angle_min = None
+angle_increment = None
+k = 10
+d = 0.5
+
+class vertice:
+    def __init__(self, center, directions):
+        self.center = center
+        self.directions = directions
+
+    def update_connected_vertice(self,dir_index,con_vertice):
+        self.directions[dir_index][1] = con_vertice
+    
+    def unexplored_directions(self):
+        unex_dir = []
+        for i in range(0,len(self.directions)):
+            if self.directions[i][1] is None:
+                unex_dir.append((i,self.directions[i][0]))
+        return unex_dir
+    
+    def is_in_vertice(self, p):
+        ep = 2*0.5
+        return np.linalg.norm(self.center-p) <= ep
+
+def odometry_callback_robot(data):
+    global q0, theta, k, d
+    orient = data.pose.pose.orientation
+    (roll, pitch, theta) = euler_from_quaternion([orient.x, orient.y, orient.z, orient.w])
+    x0 = data.pose.pose.position.x #+ d * cos(theta)
+    y0 = data.pose.pose.position.y #+ d * sin(theta)
+    q0 = np.array([x0, y0])
+
+def range_callback_robot(data):
+    global lrange, angle_min, angle_increment
+    lrange = np.array(data.ranges)
+    angle_min = data.angle_min
+    angle_increment = data.angle_increment
+
+def get_points(lrange, local_mins, q0):
+    q_list=[]
+    for i in local_mins:
+        mi_i = theta+angle_min+i*angle_increment
+        q = q0 + lrange[i]*np.array([cos(mi_i),sin(mi_i)])
+        q_list.append(q)
+    return q_list
+
+def sort_anticlockwise(q_list, q0):
+    q_list.sort(key=lambda x: np.arctan2(x[1]-q0[1],x[0]-q0[0]))
+    return q_list
+
+def line_fit(p1, p2):
+    x = [p1[0],p2[0]]
+    y = [p1[1],p2[1]]
+    return np.polyfit(x,y,1)
+
+def get_candidates(lrange, local_mins, q0):
+    assert len(local_mins) >= 2
+
+    cand_list = []
+    q_list = get_points(lrange, local_mins, q0)
+    q_list = sort_anticlockwise(q_list, q0)
+    len_q_list = len(q_list)
+    if len_q_list > 2:
+        for i in range(0,len_q_list):
+            slope,offset = line_fit(q_list[i%len_q_list],q_list[(i+1)%len_q_list])
+            grad = np.array([slope,-1])
+            grad /= np.linalg.norm(grad)
+            z = slope*q0[0] - q0[1] + offset
+            if z > 0:
+                cand_list.append(-grad)
+            elif z < 0:
+                cand_list.append(grad)
+            else:
+                cand_list.append(None)
+    else:
+        slope,offset = line_fit(q_list[0],q_list[1])
+        grad = np.array([slope,-1])
+        grad /= np.linalg.norm(grad)
+        z = slope*q0[0] - q0[1] + offset
+        if z > 0:
+            cand_list.append(-grad)
+        elif z < 0:
+            cand_list.append(grad)
+        else:
+            raise
+    return cand_list
+
+def choose_path(lrange,local_mins, history, q0, vertice_list, last_ver_idx, last_sel_dir_index, is_extreme_vertice):
+    cur_ver_idx = None
+
+    for i in range(0,len(vertice_list)): # check if vertice already exists
+        if vertice_list[i].is_in_vertice(q0):
+            cur_ver_idx = i
+            break
+
+    if cur_ver_idx is None: # new vertice found
+        cand_list = get_candidates(lrange,local_mins,q0)
+
+        if is_extreme_vertice:
+            assert len(cand_list) == 1
+            directions = [[-cand_list[0], None]]
+        else:
+            directions = []
+            for cand in cand_list:
+                directions.append([cand,None])
+
+        vertice_list.append(vertice(q0,directions))
+        cur_ver_idx = len(vertice_list)-1
+
+    unex_dirs = vertice_list[cur_ver_idx].unexplored_directions()
+    if is_extreme_vertice:
+        assert len(unex_dirs) == 1
+    if not history[history.isD1==False].empty:
+        last_reg = history[history.isD1==False].iloc[-1]
+    else:
+        last_reg = history.iloc[-1]        
+    for unex_dir in unex_dirs:
+        cand = unex_dir[1]
+        criteria = 0.5
+        l = np.array([last_reg.v_x,last_reg.v_y])
+        if last_ver_idx is not None and np.linalg.norm(-cand-l) < criteria:
+            vertice_list[cur_ver_idx].update_connected_vertice(unex_dir[0],vertice_list[last_ver_idx])
+            vertice_list[last_ver_idx].update_connected_vertice(last_sel_dir_index, vertice_list[cur_ver_idx])
+            break
+    
+    if is_extreme_vertice:
+        last_ver_idx = cur_ver_idx
+        last_sel_dir_index = 0
+        return vertice_list[cur_ver_idx].directions[0][0], True, False, vertice_list, last_ver_idx, last_sel_dir_index,last_reg, 'vertice_decision'
+    else:
+        cur_unex_dirs = vertice_list[cur_ver_idx].unexplored_directions()
+        if len(cur_unex_dirs) > 0:
+            last_ver_idx = cur_ver_idx
+            last_sel_dir_index = cur_unex_dirs[0][0]
+            return cur_unex_dirs[0][1], True, False, vertice_list, last_ver_idx, last_sel_dir_index, last_reg, 'vertice_decision'
+        else:
+            raise
+
+
+    # last_20 = history.iloc[-40:]
+    # if any(last_20.isVertice):
+    #     return np.array([last_20[last_20.isVertice == True].iloc[0].v_x,\
+    #         last_20[last_20.isVertice == True].iloc[0].v_y]), False, False
+    
+    # if not prev_choices.empty:
+    #     angle_range = 2*np.pi/180
+    #     for i in range(0,len(local_mins)):
+    #         mi_i = theta+angle_min+local_mins[i]*angle_increment-pi
+    #         di_i = np.array([cos(mi_i), sin(mi_i)])            
+    #         for j in range(1,len(local_mins)):
+    #             mi_j = theta+angle_min+local_mins[j]*angle_increment-pi
+    #             di_j = np.array([cos(mi_j), sin(mi_j)])
+    #             nil_space = null_space(np.matrix(di_i-di_j))
+    #             cand1 = nil_space[:,0]
+    #             cand2 = -nil_space[:,0]
+    #             delta_angle_c1 = abs(prev_choices.v_angle - np.arctan2(cand1[1],cand1[0]))
+    #             delta_angle_c2 = abs(prev_choices.v_angle - np.arctan2(cand2[1],cand2[0]))
+    #             if not history[history.isD1==False].empty:
+    #                 last_reg = history[history.isD1==False].iloc[-1]
+    #             else:
+    #                 last_reg = history.iloc[-1]
+    #             if not (any(delta_angle_c1 < angle_range) or any(delta_angle_c2 < angle_range)):
+    #                 if abs(np.arctan2(cand1[1],cand1[0]) - np.arctan2(last_reg.v_y,last_reg.v_x)) < \
+    #                     abs(np.arctan2(cand2[1],cand2[0]) - np.arctan2(last_reg.v_y,last_reg.v_x)):
+    #                     return cand1, True, False
+    #                 else:
+    #                     return cand2, True, False
+
+
+def get_velocity_direction1(lrange, history, q0, vertice_list, last_ver_idx, last_sel_dir_index):
+    ep = 0.05
+
+    if not history[history.isD1==False].empty:
+        last_reg = history[history.isD1==False].iloc[-1]
+    else:
+        last_reg = history.iloc[-1]
+    
+    if len(vertice_list) > 0:
+        if vertice_list[last_ver_idx].is_in_vertice(q0):
+            return vertice_list[last_ver_idx].directions[last_sel_dir_index][0], True, False,\
+                vertice_list, last_ver_idx, last_sel_dir_index, last_reg, 'in_vertice'
+    
+    ex_lrange = np.hstack((lrange,lrange))
+    lrange_local_mins = find_peaks(-ex_lrange, distance=20, prominence=0.5) # prominence=0.2 ?
+    lrange_lmins = lrange_local_mins[0]
+
+    unique_lmin = []
+    for lmin_l in lrange_lmins:
+        add_lmin = True
+        for lmin_u in unique_lmin:
+            if abs(lmin_l%len(lrange) - lmin_u%len(lrange)) < 10:
+                add_lmin = False
+                break
+        if add_lmin:
+            unique_lmin.append(lmin_l)
+    
+    unique_lmin.sort(key=lambda x: ex_lrange[x])
+    multiple_min = [ i for i in unique_lmin if abs(ex_lrange[i]-ex_lrange[unique_lmin[0]]) < ep]
+    if len(multiple_min) > 2:
+        return choose_path(ex_lrange,multiple_min, history, q0,\
+            vertice_list, last_ver_idx, last_sel_dir_index, False)
+
+    if len(unique_lmin) > 0:
+        min_fst = unique_lmin[0]
+        mi_fst = theta+angle_min+min_fst*angle_increment-pi
+        di_fst = np.array([cos(mi_fst), sin(mi_fst)])
+        if len(lrange_local_mins) > 1:
+            min_snd = unique_lmin[1]
+            if abs(ex_lrange[min_fst]-ex_lrange[min_snd]) < ep:
+                cand = get_candidates(ex_lrange,unique_lmin[0:2],q0)
+                assert len(cand) == 1
+                cand1 = cand[0]
+                cand2 = -cand[0]
+                if ex_lrange[min_fst] > 1.5 and ex_lrange[min_snd] > 1.5:
+                    l = np.array([last_reg.v_x,last_reg.v_y])
+                    if np.linalg.norm(cand1 - l) < np.linalg.norm(cand2 - l):
+                        return cand1, False, False, vertice_list, last_ver_idx, last_sel_dir_index,last_reg, 'tangent_decision'
+                    else:
+                        return cand2, False, False, vertice_list, last_ver_idx, last_sel_dir_index,last_reg, 'tangent_decision'
+                else:
+                    return choose_path(ex_lrange,multiple_min, history, q0,\
+                        vertice_list, last_ver_idx, last_sel_dir_index, True)
+        return di_fst, False, True, vertice_list, last_ver_idx, last_sel_dir_index, last_reg, 'closest'
+
+    
+# def get_velocity_direction2(lrange, history, q0):
+#     global tangent_move
+#     ep = 0.05
+    
+#     local_mins = findpeaks(method='peakdetect')
+#     results=local_mins.fit(lrange)
+#     df = results['df']
+#     if df.loc[0].valley and df.loc[len(lrange)-1].valley:
+#         df.drop(len(lrange)-1)
+    
+#     lrange_local_mins = df[df.valley == True].sort_values(by=["y"])
+#     local_min_index = lrange_local_mins.index.values
+#     #local_mins.plot()    
+
+#     if len(lrange_local_mins) > 0:
+#         min_fst = lrange_local_mins.loc[local_min_index[0]]
+#         mi_fst = theta+angle_min+min_fst.x*angle_increment-pi
+#         di_fst = np.array([cos(mi_fst), sin(mi_fst)])
+#         if len(lrange_local_mins) > 1:
+#             min_snd = lrange_local_mins.loc[local_min_index[1]]
+#             if abs(min_fst.y-min_snd.y) < ep:
+#                 mi_snd = theta+angle_min+min_snd.x*angle_increment-pi
+#                 di_snd = np.array([cos(mi_snd), sin(mi_snd)])
+#                 nil_space = null_space(np.matrix(di_fst-di_snd))
+#                 tangent_move = True
+#                 return nil_space[:,0]
+#         return di_fst
+
+def init():
+    global tangent_move, lrange
+    rospy.init_node('a_star', anonymous=True)
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+    rospy.Subscriber('/odom', Odometry, odometry_callback_robot)
+    rospy.Subscriber('/base_scan', LaserScan, range_callback_robot)
+    rate = rospy.Rate(20)
+    vel_msg = Twist()
+
+    while q0 is None:
+        continue
+
+    df = pd.DataFrame(columns=['q0_x','q0_y','v_x','v_y','v_angle','isGVD'])
+    df = df.append({'q0_x':q0[0],'q0_y':q0[1],'v_x':0,'v_y':0,'v_angle':None,\
+        'isVertice': None,'isD1':None,'isGVD':None}, ignore_index=True)
+    #last_point = df.iloc[-1]
+
+    ep = 0.005
+    alp = 0.2
+    vertice_list = []
+    last_ver_idx = None
+    last_sel_dir_index = None
+    did_not_leave_vertice = False
+    while not rospy.is_shutdown():
+        if lrange is not None:
+
+            tangent_move = False
+            V_dir,isVertice,isD1,vertice_list,last_ver_idx,last_sel_dir_index,last_regis,decision\
+                = get_velocity_direction1(lrange, df, q0, vertice_list, last_ver_idx, last_sel_dir_index)
+            #if tangent_move:
+            #    V = 2*alp*V_dir
+            #else:
+            V = alp*V_dir
+            print("v_x: {} v_y: {} | lv_x: {} lv_y: {} | Decision: {}".format(V_dir[0],V_dir[1],last_regis.v_x,last_regis.v_y, decision))
+            
+            df = df.append({'q0_x':q0[0],'q0_y':q0[1],'v_x':V_dir[0],'v_y':V_dir[1],'v_angle':np.arctan2(V_dir[1],V_dir[0]),\
+                'isVertice': isVertice,'isD1':isD1,'isGVD':tangent_move}, ignore_index=True)
+            
+            # Omnidirectional robot
+            vel_msg.linear.x = V[0]
+            vel_msg.linear.y = V[1]
+            
+            # Diff robot
+            #vel_msg.linear.x = cos(theta)*V[0]+sin(theta)*V[1]
+            #vel_msg.angular.z = (-sin(theta)*V[0]+cos(theta)*V[1])/d
+
+            rate.sleep()
+            pub.publish(vel_msg)
+    
+if __name__ == '__main__':
+    try:
+        init()
+    except rospy.ROSInterruptException:
+        pass
